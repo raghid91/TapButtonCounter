9f6b22fbf31feac0f01ed601c8373f93b14f3976
raghid91 <raghid.nami@gmail.com>
2016-12-18 21:52:18 -0500

    Initial commit

---
+++ app/src/main/java/com/cornez/tapbuttoncounter/Counter.java
@@ -0,0 +1,18 @@
+package com.cornez.tapbuttoncounter;
+
+class Counter {
+    private int mCount;
+
+    public Counter(){
+        mCount = 0;
+    }
+
+    public void addCount(){
+        mCount++;
+    }
+
+    public Integer getCount() {
+        return mCount;
+    }
+
+}
